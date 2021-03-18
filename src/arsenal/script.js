1737f7a921c2e95aeda8da4e2871e5bd72f0493d
chanayus <chanayus1526@hotmail.com>
2021-03-18 15:35:25 +0700

    update

---
+++ src/arsenal/script.js
@@ -0,0 +1,41 @@
+
+const fetchData = async() =>{
+    const data = await fetch("./player.json")
+    let player = await data.json()
+    console.log(player)
+    player.goalkeeper.map((value) => addPlayer(value, "goalkeeper"))
+    player.defender.map((value) => addPlayer(value, "defender"))
+    player.midfielder.map((value) => addPlayer(value, "midfielder"))
+    player.forward.map((value) => addPlayer(value, "forward"))
+    
+}
+
+const addPlayer = (value, role) => {
+    let card = document.createElement("div")
+    let wrapper = document.createElement("div")
+    let content = document.createElement("content")
+    let position = document.getElementById(role)
+
+    card.className = "card"
+    wrapper.className = "wrapper"
+    content.className = "content"
+
+    content.innerHTML = `
+        <h1>${value.name}</h1>
+    `
+    card.style.backgroundImage = `url("${value.img}")`
+    card.appendChild(wrapper)
+    card.appendChild(content)
+
+    position.appendChild(card)
+    console.log(position)
+
+}
+
+const appPositionPlayer = () =>{
+    console.log("goal")
+}
+
+fetchData();
+
+
